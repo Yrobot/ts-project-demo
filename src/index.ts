3cb45584c096c8502dc0b38f8c204a135b1e8360
Yrobot <yrobot@qq.com>
2022-10-18 16:49:25 +0800

    add npm import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { log } from "./utilities";
+import get from "lodash.get";
+import { UserConfig } from "next-i18next";
+
 log();
 const add = (a: number, b: number): number => a + b;
+
+console.log(get({ a: { b: { c: "abc" } } }, "a.b.c"));
+
+export type I18n = UserConfig["i18n"];
 
 export default add;
