6557180efbaae0aa2858a1a1523758c6ba43c74b
Yrobot <yrobot@qq.com>
2022-10-21 14:46:36 +0800

    add read config logic

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+import path from "path";
+const CONFIG_FILE_NAME = "next-i18next.config.js";
+
+const config = require(path.resolve(CONFIG_FILE_NAME));
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
-import { log } from "./utilities";
-import get from "lodash.get";
-import { UserConfig } from "next-i18next";
-import { resolve } from "path";
-import fs from "fs";
+// import { log } from "./utilities";
+// import get from "lodash.get";
+// import { UserConfig } from "next-i18next";
+// import fs from "fs";
 
-log();
-const add = (a: number, b: number): number => a + b;
+import config from "./config";
 
-console.log(get({ a: { b: { c: "abc" } } }, "a.b.c"));
-
-export type I18n = UserConfig["i18n"];
-
-const config = require(resolve("ts-project-demo.config.js"));
-
-fs.readdir(resolve("./"), (err, files) => {
-  if (err) {
-    throw err;
-  }
-  console.log(files);
-});
-
-export const logConfig = () => {
-  console.log("ts-project-demo.config.js", config);
-};
-
-export default add;
+console.log("config:\n", config);
